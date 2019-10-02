011d48ca2a1ec16a754c5e98df4e4dddb18932c1
ej0cl6 <ej0cl6@gmail.com>
2019-10-02 13:25:28 -0700

    ucla version

---
+++ data/peizhou.rb
@@ -0,0 +1,5 @@
+entry!('zhou2019genderlessbias',
+  emnlp(2019),
+  title('Examining Gender Bias in Languages with Grammatical Gender'),
+  author('Pei Zhou and Weijia Shi and Jieyu Zhao and Kuan-Hao Huang and Muhao Chen and Ryan Cotterell and Kai-Wei Chang'),
+nil)
---
+++ data/taomeng.rb
@@ -0,0 +1,6 @@
+entry!('meng2019constrained',
+  emnlp(2019),
+  title('Target Language-Aware Constrained Inference for Cross-lingual Dependency Parsing'),
+  author('Tao Meng and Nanyun Peng and Kai-Wei Chang'),
+nil)
+
