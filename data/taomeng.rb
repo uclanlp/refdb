f35951bf1769d827946747691a062001b4de54dc
ej0cl6 <ej0cl6@gmail.com>
2019-10-02 15:12:54 -0700

    remove

--- data/taomeng.rb
+++
@@ -1,6 +0,0 @@
-entry!('meng2019constrained',
-  emnlp(2019),
-  title('Target Language-Aware Constrained Inference for Cross-lingual Dependency Parsing'),
-  author('Tao Meng and Nanyun Peng and Kai-Wei Chang'),
-nil)
-
