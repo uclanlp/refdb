b91b3f319f4e7ff1e5439274a2053f53a4a8e416
liunian-harold-li <liunian.harold.li@gmail.com>
2019-10-03 14:45:23 -0700

    Update Harold's bib's name order

--- data/harold.rb
+++ data/harold.rb
@@ -1,10 +1,10 @@
 entry!('li2019efficient',
-    tacl(2019,volume=nil),
+    tacl(2019,volume=7),
     title('Efficient Contextual Representation Learning With Continuous Outputs'),
-    author('Li, Liunian Harold and Chen, Patrick H and Hsieh, Cho-Jui and Chang, Kai-Wei'),
+    author('Liunian Harold Li and Patrick H. Chen and Cho-Jui Hsieh and Kai-Wei Chang'),
 nil)
 entry!('li2019visualbert',
     arxiv(2019, id='1908.03557'),
     title('VisualBERT: A simple and performant baseline for vision and language'),
-    author('Li, Liunian Harold and Yatskar, Mark and Yin, Da and Hsieh, Cho-Jui and Chang, Kai-Wei'),
+    author('Liunian Harold Li and Mark Yatskar and Da Yin and Cho-Jui Hsieh and Kai-Wei Chang'),
 nil)
