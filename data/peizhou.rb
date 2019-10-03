375e7d02293d73bac7cc1f3a29b0bbaee4317a3a
ej0cl6 <ej0cl6@gmail.com>
2019-10-03 14:33:05 -0700

    update

--- data/peizhou.rb
+++ data/peizhou.rb
@@ -1,4 +1,4 @@
-entry!('zhou2019genderlessbias',
+entry!('zhou2019grammaticalgenderbias',
   emnlp(2019),
   title('Examining Gender Bias in Languages with Grammatical Gender'),
   author('Pei Zhou and Weijia Shi and Jieyu Zhao and Kuan-Hao Huang and Muhao Chen and Ryan Cotterell and Kai-Wei Chang'),
