20e2f8393fefff0c9ea06c902901d9c99703e355
wasiahmad <wasiahmad@ucla.edu>
2019-10-19 23:59:00 -0700

    files pushed

--- data/links.rb
+++ data/links.rb
@@ -4,4 +4,5 @@
 link!(
   ['Kai-Wei Chang', 'http://web.cs.ucla.edu/~kwchang/'],
   ['Kuan-Hao Huang', 'http://khhuang.net/'],
+  ['Wasi Uddin Ahmad', 'http://wasiahmad.github.io'],
 nil)
---
+++ data/wasi.rb
@@ -0,0 +1,43 @@
+entry!('ahmad2019unlabeled',
+    conll(2019),
+    title('Cross-lingual Dependency Parsing with Unlabeled Auxiliary Languages'),
+    author('Wasi Uddin Ahmad and Zhisong Zhang and Xuezhe Ma and Kai-Wei Chang and and Nanyun Peng'),
+nil)
+
+entry!('ahmad2019crosslingual',
+    naacl(2019),
+    title('On Difficulties of Cross-Lingual Transfer with Order Differences: A Case Study on Dependency Parsing'),
+    author('Wasi Uddin Ahmad and Zhisong Zhang and Xuezhe Ma and Eduard Hovy and Kai-Wei Chang and and Nanyun Peng'),
+nil)
+
+entry!('ahmad2019context',
+    sigir(2019),
+    title('Context Attentive Document Ranking and Query Suggestion'),
+    author('Wasi Uddin Ahmad and Kai-Wei Chang and Hongning Wang'),
+nil)
+
+entry!('ahmad2018multitask',
+    iclr(2018),
+    title('Multi-Task Learning for Document Ranking and Query Suggestion'),
+    author('Wasi Uddin Ahmad and Kai-Wei Chang and Hongning Wang'),
+nil)
+
+entry!('ahmad2018sentence',
+    arxiv(2018, id='1810.00681v1'),
+    title('Learning Robust, Transferable Sentence Representations for Text Classification'),
+    author('Wasi Uddin Ahmad and Xueying Bai and Nanyun Peng and Kai-Wei Chang'),
+nil)
+
+entry!('ahmad2018intent',
+    sigir(2018),
+    title('Intent-aware Query Obfuscation for Privacy Protection in Personalized Web Search'),
+    author('Wasi Uddin Ahmad and Kai-Wei Chang and Hongning Wang'),
+nil)
+
+entry!('ahmad2018nominals',
+    lrec(2018),
+    title('A Corpus to Learn Refer-to-as Relations for Nominals'),
+    author('Wasi Uddin Ahmad and Kai-Wei Chang'),
+nil)
+
+
