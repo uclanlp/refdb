6c82dd5210e64943a9be3f8c2ebed2e83dd96760
wasiahmad <wasiahmad@ucla.edu>
2019-10-20 00:11:37 -0700

    add

--- data/wasi.rb
+++ data/wasi.rb
@@ -1,43 +1,50 @@
 entry!('ahmad2019unlabeled',
-    conll(2019),
     title('Cross-lingual Dependency Parsing with Unlabeled Auxiliary Languages'),
     author('Wasi Uddin Ahmad and Zhisong Zhang and Xuezhe Ma and Kai-Wei Chang and and Nanyun Peng'),
+    conll(2019)
 nil)
 
 entry!('ahmad2019crosslingual',
-    naacl(2019),
     title('On Difficulties of Cross-Lingual Transfer with Order Differences: A Case Study on Dependency Parsing'),
     author('Wasi Uddin Ahmad and Zhisong Zhang and Xuezhe Ma and Eduard Hovy and Kai-Wei Chang and and Nanyun Peng'),
+    naacl(2019),
+    pages(2440, 2452),
+    publisher('Association for Computational Linguistics')
 nil)
 
 entry!('ahmad2019context',
-    sigir(2019),
     title('Context Attentive Document Ranking and Query Suggestion'),
     author('Wasi Uddin Ahmad and Kai-Wei Chang and Hongning Wang'),
+    sigir(2019),
+    pages(385, 394),
+    publisher('ACM'),
 nil)
 
 entry!('ahmad2018multitask',
-    iclr(2018),
     title('Multi-Task Learning for Document Ranking and Query Suggestion'),
     author('Wasi Uddin Ahmad and Kai-Wei Chang and Hongning Wang'),
+    iclr(2018)
 nil)
 
 entry!('ahmad2018sentence',
-    arxiv(2018, id='1810.00681v1'),
     title('Learning Robust, Transferable Sentence Representations for Text Classification'),
     author('Wasi Uddin Ahmad and Xueying Bai and Nanyun Peng and Kai-Wei Chang'),
+    arxiv(2018, id='1810.00681v1')
 nil)
 
 entry!('ahmad2018intent',
-    sigir(2018),
     title('Intent-aware Query Obfuscation for Privacy Protection in Personalized Web Search'),
     author('Wasi Uddin Ahmad and Kai-Wei Chang and Hongning Wang'),
+    sigir(2018),
+    pages(285, 294),
+    publisher('ACM'),
 nil)
 
 entry!('ahmad2018nominals',
-    lrec(2018),
     title('A Corpus to Learn Refer-to-as Relations for Nominals'),
     author('Wasi Uddin Ahmad and Kai-Wei Chang'),
+    lrec(2018),
+    publisher('European Language Resources Association (ELRA)')
 nil)
 
 
