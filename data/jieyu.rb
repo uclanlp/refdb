61c7de96be0d91ee2d95f5fa823d89aeab2ebb00
Veyron1110 <littlefq@qq.com>
2019-10-21 08:13:06 -0700

    jieyu's bib

---
+++ data/jieyu.rb
@@ -0,0 +1,37 @@
+entry!('zhao2017menalso',
+       emnlp(2017),
+       title('Men also like shopping: Reducing gender bias amplification using corpus-level constraints'),
+       author('Jieyu Zhao and Tianlu Wang and Mark Yatskar and Vicente Ordonez and Kai-Wei Chang'),
+       nil
+)
+entry!('zhao2018genderbiasincoref',
+       naacl(2018),
+       title('Gender Bias in Coreference Resolution: Evaluation and Debiasing Methods'),
+       author('Jieyu Zhao and Tianlu Wang and Mark Yatskar and Vicente Ordonez and Kai-Wei Chang'),
+       nil
+)
+entry!('zhao2018genderbiasincoref',
+       naacl(2018),
+       title('Gender Bias in Coreference Resolution: Evaluation and Debiasing Methods'),
+       author('Jieyu Zhao and Tianlu Wang and Mark Yatskar and Vicente Ordonez and Kai-Wei Chang'),
+       nil
+)
+entry!('zhao2018gnglove',
+       emnlp(2018),
+       title('Learning Gender-Neutral Word Embeddings'),
+       author('Jieyu Zhao and Yichao Zhou and Zeyu Li and Wei Wang and Kai-Wei Chang'),
+       nil
+)
+entry!('sun2019biasreview',
+       acl(2019),
+       title('Mitigating Gender Bias in Natural Language Processing: Literature Review'),
+       author('Tony Sun and Andrew Gaut and Shirlyn Tang and Yuxin Huang and Mai ElSherief and Jieyu Zhao and Diba Mirza and Elizabeth Belding and Kai-Wei Chang and William Yang Wang'),
+       nil
+)
+
+entry!('wang2019balancedisnot',
+       iccv(2019),
+       title('Balanced Datasets Are Not Enough: Estimating and Mitigating Gender Bias in Deep Image Representations'),
+       author('Tianlu Wang and Jieyu Zhao and Mark Yatskar and Kai-Wei Chang and Vicente Ordonez'),
+       nil
+)
